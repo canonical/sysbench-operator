e8e4d32022182664b102d13934003e558dcf4153
phvalguima <pedro.guimaraes@canonical.com>
2024-04-12 16:33:42 +0200

    [DPE-3897] Avoid CMR race condition (#21)
    [DPE-3897] Avoid CMR race condition
    
    Fix the CMR race condition and add integration tests covering the k8s use case.
    
    Closes #20
    
    ---------
    
    Co-authored-by: lucasgameiroborges <lucasgameirob@gmail.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -6,74 +6,43 @@ import asyncio
 import logging
 import re
 import subprocess
-from pathlib import Path
 from types import SimpleNamespace
 
+import juju
 import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
 from tenacity import Retrying, stop_after_delay, wait_fixed
 
+from .helpers import (
+    APP_NAME,
+    DB_CHARM,
+    DB_ROUTER,
+    DEPLOY_ALL_GROUP_MARKS,
+    DEPLOY_K8S_ONLY_GROUP_MARKS,
+    DEPLOY_VM_ONLY_GROUP_MARKS,
+    DURATION,
+    K8S_DB_MODEL_NAME,
+)
+
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = METADATA["name"]
-MYSQL_APP_NAME = "mysql"
-PGSQL_APP_NAME = "postgresql"
-DURATION = 10
+
+model_db = None
 
 
-DB_CHARM = {
-    "mysql": {
-        "charm": "mysql",
-        "channel": "8.0/edge",
-        "config": {"profile": "testing"},
-        "app_name": MYSQL_APP_NAME,
-    },
-    "postgresql": {
-        "charm": "postgresql",
-        "channel": "14/edge",
-        "config": {},
-        "app_name": PGSQL_APP_NAME,
-    },
-}
-
-
-DB_ROUTER = {
-    "mysql": {
-        "charm": "mysql-router",
-        "channel": "dpe/edge",
-        "config": {},
-        "app_name": "mysql-router",
-    },
-    "postgresql": {
-        "charm": "pgbouncer",
-        "channel": "1/edge",
-        "config": {},
-        "app_name": "pgbouncer",
-    },
-}
-
-
-DEPLOY_ALL_GROUP_MARKS = [
-    (
-        pytest.param(
-            app,
-            router,
-            id=f"{app}_router-{router}",
-            marks=pytest.mark.group(f"{app}_router-{router}"),
+def check_service(svc_name: str, retry_if_fail: bool = True):
+    if not retry_if_fail:
+        return subprocess.check_output(
+            ["juju", "ssh", f"{APP_NAME}/0", "--", "sudo", "systemctl", "is-active", svc_name],
+            text=True,
         )
-    )
-    for app in ["mysql", "postgresql"]
-    for router in [True, False]
-]
-
-
-def check_service(svc_name):
-    return subprocess.check_output(
-        ["juju", "ssh", f"{APP_NAME}/0", "--", "sudo", "systemctl", "is-active", svc_name],
-        text=True,
-    )
+    for attempt in Retrying(stop=stop_after_delay(150), wait=wait_fixed(15)):
+        with attempt:
+            return subprocess.check_output(
+                ["juju", "ssh", f"{APP_NAME}/0", "--", "sudo", "systemctl", "is-active", svc_name],
+                text=True,
+            )
 
 
 async def run_action(
@@ -87,10 +56,108 @@ async def run_action(
     return SimpleNamespace(status=result.status or "completed", response=result.results)
 
 
-@pytest.mark.parametrize("db_driver,use_router", DEPLOY_ALL_GROUP_MARKS)
+@pytest.fixture(scope="module", autouse=True)
+async def destroy_model_in_k8s(ops_test, microk8s):
+    yield
+
+    if ops_test.keep_model:
+        return
+    controller = juju.controller.Controller()
+    await controller.connect()
+    await controller.destroy_model(K8S_DB_MODEL_NAME)
+    await controller.disconnect()
+    ctlname = list(yaml.safe_load(subprocess.check_output(["juju", "show-controller"])).keys())[0]
+
+    subprocess.run(["sudo", "snap", "remove", "--purge", "microk8s"], check=True)
+    subprocess.run(["sudo", "snap", "remove", "--purge", "kubectl"], check=True)
+    subprocess.run(
+        ["juju", "remove-cloud", "--client", "--controller", ctlname, microk8s.cloud_name],
+        check=True,
+    )
+
+
+@pytest.mark.parametrize("db_driver,use_router", DEPLOY_K8S_ONLY_GROUP_MARKS)
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_build_and_deploy(ops_test: OpsTest, db_driver, use_router) -> None:
+async def test_build_and_deploy_k8s_only(
+    ops_test: OpsTest, microk8s, db_driver, use_router
+) -> None:
+    """Build the charm and deploy + 3 db units to ensure a cluster is formed."""
+    # Create a new model for DB on k8s:
+    logging.info(f"Creating k8s model {K8S_DB_MODEL_NAME}")
+    controller = juju.controller.Controller()
+    await controller.connect()
+    await controller.add_model(K8S_DB_MODEL_NAME, cloud_name=microk8s.cloud_name)
+
+    global model_db
+    model_db = juju.model.Model()
+    await model_db.connect(model_name=K8S_DB_MODEL_NAME)
+
+    await model_db.deploy(
+        DB_CHARM[db_driver]["charm"],
+        application_name=DB_CHARM[db_driver]["app_name"],
+        num_units=3,
+        channel=DB_CHARM[db_driver]["channel"],
+        config=DB_CHARM[db_driver]["config"],
+        trust=True,
+    )
+    if use_router:
+        await model_db.deploy(
+            DB_ROUTER[db_driver]["charm"],
+            application_name=DB_ROUTER[db_driver]["app_name"],
+            channel=DB_ROUTER[db_driver]["channel"],
+            config=DB_ROUTER[db_driver]["config"],
+            trust=True,
+        )
+        await model_db.relate(
+            f"{DB_CHARM[db_driver]['app_name']}:database",
+            f"{DB_ROUTER[db_driver]['app_name']}",
+        )
+
+    # Now, set up the sysbench and relate to the CMR
+    charm = await ops_test.build_charm(".")
+    config = {
+        "threads": 1,
+        "tables": 1,
+        "scale": 1,
+        "duration": 0,
+    }
+    await ops_test.model.deploy(
+        charm,
+        application_name=APP_NAME,
+        num_units=1,
+        config=config,
+    )
+
+    if use_router:
+        await model_db.create_offer(
+            endpoint="database",
+            offer_name="database",
+            application_name=DB_ROUTER[db_driver]["app_name"],
+        )
+    else:
+        await model_db.create_offer(
+            endpoint="database",
+            offer_name="database",
+            application_name=DB_CHARM[db_driver]["app_name"],
+        )
+    await ops_test.model.consume(f"admin/{model_db.name}.database")
+    await ops_test.model.relate("database", f"{APP_NAME}:{DB_CHARM[db_driver]['app_name']}")
+
+    # Reduce the update_status frequency until the cluster is deployed
+    async with ops_test.fast_forward("60s"):
+        await ops_test.model.block_until(
+            lambda: len(ops_test.model.applications[APP_NAME].units) == 1
+        )
+    await model_db.wait_for_idle(status="active")
+    await controller.disconnect()
+    await model_db.disconnect()
+
+
+@pytest.mark.parametrize("db_driver,use_router", DEPLOY_VM_ONLY_GROUP_MARKS)
+@pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
+async def test_build_and_deploy_vm_only(ops_test: OpsTest, db_driver, use_router) -> None:
     """Build the charm and deploy + 3 db units to ensure a cluster is formed."""
     charm = await ops_test.build_charm(".")
 
@@ -136,16 +203,22 @@ async def test_build_and_deploy(ops_test: OpsTest, db_driver, use_router) -> Non
         )
 
     # Reduce the update_status frequency until the cluster is deployed
+    apps = [DB_CHARM[db_driver]["app_name"]]
+    if use_router:
+        apps.append(DB_ROUTER[db_driver]["app_name"])
     async with ops_test.fast_forward("60s"):
         await ops_test.model.block_until(
             lambda: len(ops_test.model.applications[APP_NAME].units) == 1
         )
         await ops_test.model.wait_for_idle(
-            apps=[DB_CHARM[db_driver]["app_name"]],
+            apps=apps,
             status="active",
-            raise_on_blocked=True,
-            timeout=15 * 60,
+            timeout=30 * 60,
         )
+
+    # set the model to the global model_db
+    global model_db
+    model_db = ops_test.model
 
 
 @pytest.mark.parametrize("db_driver,use_router", DEPLOY_ALL_GROUP_MARKS)
@@ -240,25 +313,29 @@ async def test_run_action(ops_test: OpsTest, db_driver, use_router) -> None:
     assert output.status == "completed"
 
     svc_output = check_service("sysbench.service")
+    logger.info(f"sysbench.service output: {svc_output}")
+
     # Looks silly, but we "active" is in "inactive" string :(
     assert "inactive" not in svc_output and "active" in svc_output
-    # Wait until it is finished, and retry
-    await asyncio.sleep(3 * DURATION)
 
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME],
-        status="active",
-        raise_on_blocked=True,
-        timeout=15 * 60,
-    )
+    async with ops_test.fast_forward("60s"):
+        # Wait until it is finished, and retry
+        await asyncio.sleep(3 * DURATION)
 
-    try:
-        svc_output = check_service("sysbench.service")
-    except subprocess.CalledProcessError:
-        # Finished running and check_output for "systemctl is-active" will fail
-        return
-    # Did not fail, so check if we got a "inactive" in the output
-    assert "inactive" in svc_output
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            timeout=15 * 60,
+        )
+
+        try:
+            logger.info("Checking if sysbench.service is inactive")
+            svc_output = check_service("sysbench.service", retry_if_fail=False)
+        except subprocess.CalledProcessError:
+            # Finished running and check_output for "systemctl is-active" will fail
+            return
+        # Did not fail, so check if we got a "inactive" in the output
+        assert "inactive" in svc_output
 
 
 @pytest.mark.parametrize("db_driver,use_router", DEPLOY_ALL_GROUP_MARKS)
@@ -269,22 +346,17 @@ async def test_clean_action(ops_test: OpsTest, db_driver, use_router) -> None:
     assert output.status == "completed"
 
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, DB_CHARM[db_driver]["app_name"]],
+        apps=[APP_NAME],
         status="active",
         raise_on_blocked=True,
         timeout=15 * 60,
     )
-    try:
-        svc_output = check_service("sysbench.service")
-    except subprocess.CalledProcessError:
-        # Finished running and check_output for "systemctl is-active" will fail
-        pass
-    else:
-        assert "inactive" in svc_output
 
-    try:
-        svc_output = check_service("sysbench_prepared.target")
-    except subprocess.CalledProcessError:
-        # Finished running and check_output for "systemctl is-active" will fail
-        return
-    assert "inactive" in svc_output
+    for svc_name in ["sysbench.service", "sysbench_prepared.target"]:
+        try:
+            svc_output = check_service(svc_name, retry_if_fail=False)
+        except subprocess.CalledProcessError:
+            # Finished running and check_output for "systemctl is-active" will fail
+            pass
+        else:
+            assert "inactive" in svc_output
